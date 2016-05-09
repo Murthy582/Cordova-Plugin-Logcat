6a6d2c85ae3f5ae0943b77e64cc511f55124f489
Murthy VVR <murthy.582@gmail.com>
2016-05-09 15:54:45 +0530

    iOS changes

--- src/ios/LogCat.swift
+++ src/ios/LogCat.swift
@@ -1,19 +1,37 @@
-#import "Bridging-Header.h"
 
-@implementation LogCat
+import Foundation
 
-- (void)sendLogs:(CDVInvokedUrlCommand*)command
-{
-
-    NSString* callbackId = [command callbackId];
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
-    [self success:result callbackId:callbackId];
+@objc(HWPHello) class Hello : CDVPlugin {
+    func greet(command: CDVInvokedUrlCommand) {
+        let message = command.arguments[0] as! String
+        var logs = [[String: String]]()
+        logs = systemLogs()
+        print(logs)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello \(message)")
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
 }
 
-@end
+func systemLogs() -> [[String: String]] {
+    let q = asl_new(UInt32(ASL_TYPE_QUERY))
+    var logs = [[String: String]]()
+    let r = asl_search(nil, q)
+    var m = asl_next(r)
+    while m != nil {
+        var logDict = [String: String]()
+        var i: UInt32 = 0
+        while true {
+            if let key = String.fromCString(asl_key(m, i)) {
+                let val = String.fromCString(asl_get(m, key))
+                logDict[key] = val
+                i++
+            } else {
+                break
+            }
+        }
+        m = asl_next(r)
+        logs.append(logDict)
+    }
+    asl_release(r)
+    return logs
+}
