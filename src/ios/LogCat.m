f0484ce09223e5fea6fe6a190e6bbb95f1937b63
Murthy VVR <murthy.582@gmail.com>
2016-05-10 16:21:43 +0530

    Objective C Implementation

--- src/ios/LogCat.m
+++ src/ios/LogCat.m
@@ -8,5 +8,11 @@
   NSDictionary* options = [command argumentAtIndex:0];
 
   NSString *message  = options[@"message"];
+    NSArray *allPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [allPaths objectAtIndex:0];
+    NSString *pathForLog = [documentsDirectory stringByAppendingPathComponent:@"logcat.txt"];
+    
+    freopen([pathForLog cStringUsingEncoding:NSASCIIStringEncoding],"a+",stderr);
   
   }
+@end
