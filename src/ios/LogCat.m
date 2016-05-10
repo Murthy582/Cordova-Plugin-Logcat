cf647bb0119c2629e902e9020e26131d9604516d
Murthy VVR <murthy.582@gmail.com>
2016-05-10 17:35:14 +0530

    Removing unnecessary code

--- src/ios/LogCat.m
+++ src/ios/LogCat.m
@@ -5,9 +5,6 @@
 
 - (void)sendLogs:(CDVInvokedUrlCommand*)command {
 
-  NSDictionary* options = [command argumentAtIndex:0];
-
-  NSString *message  = options[@"message"];
     NSArray *allPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [allPaths objectAtIndex:0];
     NSString *pathForLog = [documentsDirectory stringByAppendingPathComponent:@"logcat.txt"];
